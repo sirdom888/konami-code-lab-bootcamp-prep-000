7ec120a524526407be573cdf1a896b34e4fda87f
Dom Siravo <domcode88@gmail.com>
2020-11-19 21:59:24 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -13,7 +13,17 @@ const codes = [
 
 function init() {
   // your code here
+  
+ let index = 0;
   document.body.addEventListener("keydown", (event) => {
+    
+    const key = event.key;
+    
+    if (key === codes[index]) {
+      index++;
+      
+      if (index)
+    }
     
   });
 }
